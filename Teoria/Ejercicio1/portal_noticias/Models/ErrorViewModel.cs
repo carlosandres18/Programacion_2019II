25c2a7b0bb5cad84571a9d3e27e564020ba0bdbb
Carlos Oroz <carlos_oroz@usmp.pe>
2019-09-10 19:04:23 -0500

    Añadir cambios del 10/09/19

---
+++ Teoria/Ejercicio1/portal_noticias/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace portal_noticias.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
