9c9565ce34d1de05f68199e625f1edaca23d28ef
Carlos Oroz <carlos_oroz@usmp.pe>
2019-09-15 18:37:30 -0500

    Añadidos cambios intermedios

---
+++ Laboratorio/Ejercicio_03/student_app/Models/student.cs
@@ -0,0 +1,17 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace student_app.models{
+    public class student{
+        public int id {get;set;}
+        public String nombre {get;set;}
+        
+        [DataType(DataType.Date)]
+        public DateTime birth {get;set;}
+
+        public String hobbie {get;set;}
+        public String curso {get;set;}
+        public Int credito {get;set;}
+        public Double total {get;set;}
+    }
+}
