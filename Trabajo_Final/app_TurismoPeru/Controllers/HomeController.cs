e5b7e13620edab9d0254614b3916077a5ea2c6de
Carlos Oroz <carlos_oroz@usmp.pe>
2019-09-26 19:29:14 -0500

    Cambios relacionados al proyecto

---
+++ Trabajo_Final/app_TurismoPeru/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using app_TurismoPeru.Models;
+
+namespace app_TurismoPeru.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Nosotros()
+        {
+
+            return View();
+        }
+
+    }
+}
